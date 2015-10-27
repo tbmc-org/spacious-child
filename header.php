ccadaf5b37801cd314d738c457c6a0fb8778934c
Keith Wickramasekara <keith.wi@live.com>
2015-10-26 19:55:36 -0500

    Added GA code

--- header.php
+++ header.php
@@ -34,6 +34,15 @@ wp_head();
 </head>
 
 <body <?php body_class(); ?>>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-68874623-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 <?php	do_action( 'before' ); ?>
 <div id="page" class="hfeed site">
 	<?php do_action( 'spacious_before_header' ); ?>
