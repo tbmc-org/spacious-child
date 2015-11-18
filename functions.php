a7eeb169b985bff392cef57e1f56c6a247024008
Keith Wickramasekara <keith.wi@live.com>
2015-11-17 22:17:32 -0600

    Removed Bootstrap

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@
 function theme_enqueue_styles() {
 	$parent_style = 'parent-style';
 
-	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/assets/css/bootstrap-custom.css' );
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
 }
